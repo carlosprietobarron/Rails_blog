47a3ff9b807270425700c05ac412c37dc86d4785
Johann <themrblacksapce@hotmail.com>
2020-05-07 14:01:40 -0600

    Flash added and root route seted

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,4 @@
-class ArticlesController < ApplicationController 
+class ArticlesController < ApplicationController
   include ArticlesHelper
   def index
     @articles = Article.all
@@ -12,6 +12,9 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+
+    flash.notice = "Article '#{@article.title}' Created"
+
     redirect_to article_path(@article)
   end
 
@@ -23,12 +26,17 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.update(article_params)
 
+    flash.notice = "Article '#{@article.title}' Updated!"
+
     redirect_to article_path(@article)
   end
-  
+
   def destroy
     @article=Article.find(params[:id])
     @article.destroy
+
+    flash.notice = "Article '#{@article.title}' Deleted"
+
     redirect_to articles_path
   end
 end
